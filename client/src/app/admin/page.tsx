92db0f7623650e3eb4ea28b9535f13b5ef5dabe4
Zach Riane Machacon <zachriane01@gmail.com>
2024-03-29 15:53:28 +0800

    feat: implemented data table for admin

--- client/src/app/admin/page.tsx
+++ client/src/app/admin/page.tsx
@@ -1,9 +1,51 @@
 import { Navbar } from "@/components/admin/home/Navbar";
+import { DataTable } from "@/components/admin/home/user-table/DataTable";
+import { columns } from "@/components/admin/home/user-table/columns";
+import { User } from "@/types/types";
+
+function getData(): User[] {
+  // Fetch data from your API here.
+  return [
+    {
+      idNumber: "18020919",
+      userName: "Zach Riane",
+      address: "sad",
+      contactNumber: "09173198899",
+      email: "18020919@usc.edu.ph",
+    },
+    {
+      idNumber: "1212121212",
+      userName: "Zach Riane",
+      address: "sad",
+      contactNumber: "09173198899",
+      email: "18020919@usc.edu.ph",
+    },
+    {
+      idNumber: "18020919",
+      userName: "Zach Riane",
+      address: "sad",
+      contactNumber: "09173198899",
+      email: "18020919@usc.edu.ph",
+    },
+    {
+      idNumber: "18020919",
+      userName: "Zach Riane",
+      address: "sad",
+      contactNumber: "09173198899",
+      email: "18020919@usc.edu.ph",
+    },
+    // ...
+  ];
+}
 
 export const Page = () => {
+  const data = getData();
   return (
     <>
-      <Navbar />
+      <div className="flex flex-col">
+        <Navbar />
+        <DataTable columns={columns} data={data} />
+      </div>
     </>
   );
 };
---
+++ client/src/components/admin/home/user-table/columns.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { User } from "@/types/types";
+import { MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export const columns: ColumnDef<User>[] = [
+  {
+    accessorKey: "idNumber",
+    header: "ID Number",
+  },
+  {
+    accessorKey: "userName",
+    header: "Name",
+  },
+  {
+    accessorKey: "address",
+    header: "Address",
+  },
+  {
+    accessorKey: "contactNumber",
+    header: "Contact Number",
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const user = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(user.idNumber)}
+            >
+              Copy ID number
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Edit</DropdownMenuItem>
+            <DropdownMenuItem>Delete</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
